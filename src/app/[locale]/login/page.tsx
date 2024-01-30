fd5ff18525b4cf79b368c6aca2fb0cc340120c63
ArtemPaskall <artempaskal@gmail.com>
2024-01-30 22:11:36 +0200

    start login

--- src/app/[locale]/login/page.tsx
+++ src/app/[locale]/login/page.tsx
@@ -4,8 +4,6 @@ import { useI18n } from '../../../../locales/client'
 
 export default function Login() {
   const t = useI18n()
-  console.log('t', t)
-  console.log('useI18n', useI18n)
 
   return (
     <div className={styles['login-wrapper']}>
